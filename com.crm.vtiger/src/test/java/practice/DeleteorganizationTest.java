2abb0e6115ff1a5abcdd1a5862d0a48ed41ff7db
LOVERBOY <LOVERBOY@muralim>
2022-03-23 17:32:54 +0530

    second commit message

---
+++ com.crm.vtiger/src/test/java/practice/DeleteorganizationTest.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class DeleteorganizationTest {
+@Test
+public void deleteorganization()
+{
+	System.out.println("organization deleted successfully");
+}
+}
